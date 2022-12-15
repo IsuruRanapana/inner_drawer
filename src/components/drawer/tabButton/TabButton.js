32d5d55a1b90a415e1bb423c30f310634e648a04
IsuruRanapana <113498635+IsuruRanapana@users.noreply.github.com>
2022-12-15 11:01:06 +0530

    Changed the drawer name and configured user profile section in the drawer menu

---
+++ src/components/drawer/tabButton/TabButton.js
@@ -0,0 +1,43 @@
+// *** Created By Isuru B. Ranapana *** //
+// ***     Eyepax IT Consulting     *** //
+// ***   on 12/14/2022 => 1:27 PM  *** //
+
+
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+
+export default function TabButton({currentTab, setCurrentTab, title, image}) {
+    return (
+        <TouchableOpacity onPress={() => {
+            if (title === 'LogOut') {
+                // todo: Logout function should be implemented
+            } else {
+                setCurrentTab(title);
+            }
+        }}>
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingVertical: 8,
+                backgroundColor: currentTab === title ? 'white' : 'transparent',
+                paddingLeft: 13,
+                paddingRight: 35,
+                borderRadius: 8,
+                marginTop: 15,
+            }}>
+
+                <Image source={image} style={{
+                    width: 25, height: 25,
+                    tintColor: currentTab === title ? '#5359D1' : 'white',
+                }}></Image>
+
+                <Text style={{
+                    fontSize: 15,
+                    fontWeight: 'bold',
+                    paddingLeft: 15,
+                    color: currentTab === title ? '#5359D1' : 'white',
+                }}>{title}</Text>
+
+            </View>
+        </TouchableOpacity>
+    );
+}
